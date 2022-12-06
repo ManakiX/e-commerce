766ab513fb2e63e8fc1b9a6ce20fda3c97fc8846
ManakiX <sogeyanaluntu@gmail.com>
2022-12-06 14:40:46 +0200

    update

---
+++ data/code.js
@@ -0,0 +1,28 @@
+// Sample data: example
+let products = JSON.parse(localStorage.getItem('products ')) ?
+JSON.parse(localStorage.getItem('products ')) : [
+    {
+        id: 1,
+        productName: "Luffy  Jorden "
+    },
+    {
+        id: 1,
+        productName: " Zoro Jorden"
+    },
+    {
+        id: 1,
+        productName: "Luffy Straw-Hat"
+    },
+    {
+        id: 1,
+        productName: "Zoro Sword"
+    },
+    {
+        id: 1,
+        productName: "Chopper Hat"
+    },
+    {
+        id: 1,
+        productName: "Boat Figurine"
+    }
+];
