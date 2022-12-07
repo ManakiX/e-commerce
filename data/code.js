dd94933c6dc9a6457d58ce024d58135220480fe2
ManakiX <sogeyanaluntu@gmail.com>
2022-12-07 10:58:29 +0200

    fixed row

--- data/code.js
+++ data/code.js
@@ -3,26 +3,38 @@ let products = JSON.parse(localStorage.getItem('products ')) ?
 JSON.parse(localStorage.getItem('products ')) : [
     {
         id: 1,
-        productName: "Luffy  Jorden "
+        productName: "Luffy Jorden ",
+        Price: "3 000",
+        brand:"Jordens",
     },
     {
-        id: 1,
-        productName: " Zoro Jorden"
+        id: 2,
+        productName: " Zoro Jorden",
+        Price: "3 00",
+        brand:"Jordens",
     },
     {
-        id: 1,
-        productName: "Luffy Straw-Hat"
+        id: 3,
+        productName: "Luffy Straw-Hat",
+        Price:"500",
+        Brand:"One Piece",
     },
     {
-        id: 1,
-        productName: "Zoro Sword"
+        id: 4,
+        productName: "Zoro Sword",
+        Price:"1 00",
+        Brand:"One Piece",
     },
     {
-        id: 1,
-        productName: "Chopper Hat"
+        id: 5,
+        productName: "Chopper Hat",
+        Price:"500"
+ ,      Brand:"One Piece",
     },
     {
-        id: 1,
-        productName: "Boat Figurine"
+        id: 6,
+        productName: "Boat Figurine",
+        Price:"250",
+        Brand:"One Piece",
     }
 ];
